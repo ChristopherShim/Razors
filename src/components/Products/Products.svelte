aefc36cd58a864af66cfb7827375b9585bcdf9f4
Ratatouille <cshim201@gmail.com>
2022-03-28 23:10:26 -0400

    Added Loading Gif
    If items are not yet reachable, it will play a Loading Gif instead.

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,8 +1,9 @@
 <script>
     export let title = '';
     import {onMount, onDestroy} from 'svelte';
-    import products from '../../stores/defaultProducts'
+    import products from '../../stores/defaultProducts';
     import Product from './Product.svelte';
+    import Loading from '../Loading.svelte';
 
     // let localProducts = [];
     // let unsubscribe;
@@ -24,6 +25,8 @@
     <div class="products-center">
         {#each $products as product (product.id)}
         <Product {product} />
+        {:else}
+        <Loading />
         {/each}
     </div>
 </section>
