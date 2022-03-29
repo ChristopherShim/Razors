5a908050d1e348d8954eced6b8c6eb852aa21721
Ratatouille <cshim201@gmail.com>
2022-03-28 22:47:31 -0400

    Products Component Additions
    Created a new 'Product' component with dummy data for now.
    
    Inside 'Products' component, added a section/h2/div wrapped around our #each function for our products.
    
    Provided our #each function with a key (product.id) for future use.

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,20 +1,29 @@
 <script>
+    export let title = '';
     import {onMount, onDestroy} from 'svelte';
     import products from '../../stores/defaultProducts'
-    let localProducts = [];
+    import Product from './Product.svelte';
 
-    onMount(()=>{
-        products.subscribe(value => {
-            console.log(value)
-            localProducts = value;
-        });
-    });
+    // let localProducts = [];
+    // let unsubscribe;
+    // onMount(()=>{
+    //     unsubscribe = products.subscribe(value => {
+    //         console.log('subscribe')
+    //         localProducts = value;
+    //     });
+    // });
+
+    // onDestroy(()=>{
+    //     unsubscribe()
+    // })
+
+
 </script>
-
-<h1>
-    hello from Pdocuts component
-</h1>
-
-{#each localProducts as product}
-<h2>{product.title}</h2>
-{/each}
+<section class='section'>
+    <h2 class='section-title'>{title}</h2>
+    <div class="products-center">
+        {#each $products as product (product.id)}
+        <Product {product} />
+        {/each}
+    </div>
+</section>
