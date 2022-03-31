da1f349fd28a5c18f28bae98c713c2a53c5adabe
Ratatouille <cshim201@gmail.com>
2022-03-31 19:17:21 -0400

    Cart.svelte card-footer update with Login or Checkout button logic
    Inside Cart.svelte, created if logic for if User is true, it will show a checkout button and redirect to the checkout page.
    
    If User is false, then it will show a message 'In Order To Checkout LOGIN' and Login button will redirect you to Login Page.

--- src/components/Cart/Cart.svelte
+++ src/components/Cart/Cart.svelte
@@ -9,6 +9,7 @@
 <div class="cart-overlay" transition:blur>
     <div class="cart-container" transition:fly={{x:100}}>
         <div class="cart" transition:fade={{delay:400}}>
+            <!-- cart header -->
             <div class="cart-header">
                 <button class='btn-close' on:click={()=> {
                 globalStore.toggleItem('cart', false)}}>
@@ -17,6 +18,27 @@
                 <h2 class='cart-title'>Your Bag</h2>
                 <span></span>
             </div>
+            <!-- end of cart header -->
+            <!-- cart items -->
+            <p>cart items</p>
+            <!-- end of cart items -->
+            <!-- cart footer -->
+            <div class="cart-footer">
+                {#if user}
+                <a href='/checkout' use:link class='btn btn-primary btn-block' 
+                on:click={()=> {
+                    globalStore.toggleItem('cart', false)
+                    }}>checkout</a>
+                {:else}
+                <p class='cart-login'>In order to checkout 
+                    <a href='/login' use:link  
+                    on:click={()=> {
+                        globalStore.toggleItem('cart', false)
+                        }}>login</a>    
+                </p>
+                {/if}
+            </div>
+            <!-- end of cart footer -->
         </div>
 
     </div>
