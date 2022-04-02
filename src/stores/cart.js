afd129329882d3687bc95e7ebb0855c257c35ae3
Ratatouille <cshim201@gmail.com>
2022-04-01 20:35:18 -0400

    changed our static const cart to dynamic cart
    title

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,7 +1,7 @@
 import {writable,derived} from 'svelte/store';
-import localCart from '../localCart';
+// import localCart from '../localCart';
 // cart
-const cart = writable([...localCart])
+const cart = writable(getStorageCart())
 // cart total
 
 export const cartTotal = derived(cart,($cart)=>{
@@ -60,7 +60,6 @@ export const decreaseAmount = id => {
         return [...cart]
     })
 }
-// localStorage
 
 export const addToCart = product => {
     cart.update(storeValue => {
@@ -76,5 +75,14 @@ export const addToCart = product => {
         }
         return cart
     })
+}
+
+// localStorage
+function getStorageCart(){
+    return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[]
+}
+
+export function setStorageCart(cartValues){
+    localStorage.setItem('cart',JSON.stringify(cartValues))
 }
 export default cart;
