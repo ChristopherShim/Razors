17488a35be3a8b679fe012545d1b35d9bfc38c31
Ratatouille <cshim201@gmail.com>
2022-04-04 20:09:13 -0400

    products.js failed implementation
    Course author messed up lectures

--- src/components/Products/Featured.svelte
+++ src/components/Products/Featured.svelte
@@ -1,21 +1,19 @@
 <script>
-    export let title = '';
-    import products, {featuredStore} from '../../stores/defaultProducts';
-    import Product from './Product.svelte';
-    import Loading from '../Loading.svelte';
-    // $: featured = $products.filter(item => item.featured === true)
-
-
-</script>
-{#if $featuredStore.length === 0}
-<Loading/>
-{:else}
-<section class='section'>
-    <h2 class='section-title'>{title}</h2>
-    <div class="products-center">
+    export let title = "";
+    import products, { featuredStore } from "../../stores/defaultProducts";
+    import Product from "./Product.svelte";
+    import Loading from "../Loading.svelte";
+  </script>
+  
+  {#if $featuredStore.length === 0}
+    <Loading />
+  {:else}
+    <section class="section">
+      <h2 class="section-title">{title}</h2>
+      <div class="products-center">
         {#each $featuredStore as product (product.id)}
-        <Product {product} />
+          <Product {product} />
         {/each}
-    </div>
-</section>
-{/if}
+      </div>
+    </section>
+  {/if}
