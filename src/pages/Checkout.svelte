4704cbc2fb6796eff74e1913441e5759100689c2
Ratatouille <cshim201@gmail.com>
2022-04-07 18:42:33 -0400

    basic checkout form for name only
    inside Checkout.svelte, created basic form for submitting name.
    
    Added button logic for if user entered in a name or not, button disabled if nothing written, enabled when something is written inside.

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -4,15 +4,44 @@
     import user from '../stores/user';
     import {cartTotal} from '../stores/cart';
 
+    let name = '';
+    $: isEmpty = !name;
+
     onMount(()=>{
         if(!$user.jwt){
             navigate('/')
         }
     })
+
+    function handleSubmit(){
+        console.log('form submitted')
+    }
 </script>
 
 {#if $cartTotal > 0}
-<h1>form</h1>
+<section class="form">
+    <h2 class="section-title">checkout</h2>
+    <form class="checkout-form" 
+    on:submit|preventDefault={handleSubmit}>
+    <h3>order total: ${$cartTotal}</h3>
+    <!-- single input -->
+    <div class="form-control">
+        <label for='name'>your name</label>
+        <input type="text" name="name" bind:value={name}>
+    </div>
+    <!-- end of single input -->
+
+    <!-- stripe stuff -->
+    <!-- end of stripe stuff -->
+
+    <!-- error message -->
+    {#if isEmpty}
+    <p class='form-empty'>please fill out name field</p>
+    {/if}
+    <!-- submit button -->
+    <button type='submit' class='btn btn-block btn-primary' disabled={isEmpty} class:disabled={isEmpty}>submit</button>
+</form>
+</section>
 {:else}
 <div class="checkout-empty">
     <h2>your cart is empty</h2>
